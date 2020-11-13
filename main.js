c836939416d19f58bbf028b5f337b0d3a8bab45e
coder2711 <73233141+coder2711@users.noreply.github.com>
2020-11-13 22:44:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function look() {
+    document.getElementById("switch").src="11-13-2020-22.08.07(1).jpg";
+}
+function nolook() {
+    document.getElementById("switch").src="11-13-2020-22.08.07.jpg";
+}
